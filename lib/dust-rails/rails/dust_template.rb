5ec49d39faf55d94ce43f95660bc6c6c20d0bdc5
Haim Lankry <HaimLankry@gmail.com>
2014-02-07 14:19:03 +0200

    Add false to compile , to try keep newlines

--- lib/dust-rails/rails/dust_template.rb
+++ lib/dust-rails/rails/dust_template.rb
@@ -31,7 +31,7 @@ module Dust
       def evaluate(scope, locals, &block)
         template_root = Dust.config.template_root
         template_name = file.split(template_root).last.split('.',2).first
-        Source.context.call("dust.compile", data, template_name)
+        Source.context.call("dust.compile", data, template_name, false)
       end
     end
   end
